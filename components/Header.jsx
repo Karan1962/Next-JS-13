5ef49e4137d29263f7da25caa48a2e9ca6b1f237
karan <karansawan5050@gmail.com>
2024-09-26 14:36:40 +0530

    routing and auth bug fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,34 @@
+"use client";
 import React from "react";
-import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const Header = () => {
+  const { user } = useUser();
   return (
     <nav className="flex justify-between px-10">
       <ul>
         <li>Logo</li>
       </ul>
       <ul className="flex w-1/2 justify-between">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
         <li>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
+          <Link href={"/"}>Home</Link>
         </li>
         <li>
-          <UserButton />
+          <Link href={"/about"}>About</Link>
         </li>
+        <li>
+          <Link href={"/contact"}>Contact</Link>
+        </li>
+        {user ? (
+          <li>
+            <UserButton />
+          </li>
+        ) : (
+          <li>
+            <Link href={"/sign-in"}>Sign In</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
