9aabf6e404c42e266bee1f6993bf2dbd4bd67201
karan <karansawan5050@gmail.com>
2024-09-27 15:37:01 +0530

    minor change in css

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center text-4xl text-red-500">
+    <main className="flex min-h-screen w-full items-center justify-center">
       {children}
     </main>
   );
