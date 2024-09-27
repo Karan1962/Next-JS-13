187160c89c6eb7be8aff988fa084aec27dc5b6d5
karan <karansawan5050@gmail.com>
2024-09-27 16:13:17 +0530

    add asset and layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,29 @@ import { ClerkProvider } from "@clerk/nextjs";
 import Header from "../components/Header.jsx";
 import "./globals.css";
 import { dark } from "@clerk/themes";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    " A comminity driven platform for asking and answering programming questions",
+  icons: {
+    icon: "/devflow_public/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -12,10 +35,14 @@ export default function RootLayout({
       appearance={{
         baseTheme: dark,
         variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+        elements: {
+          formButtonPrimary: "primary-text-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
       }}
     >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <Header />
           {children}
         </body>
