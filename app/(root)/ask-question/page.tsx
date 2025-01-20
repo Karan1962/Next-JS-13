d9300832f5edeaab711cc0702f7254ad666e824a
karan <karansawan5050@gmail.com>
2025-01-21 02:51:49 +0530

    add editor

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,14 @@
+import Question from "@/components/shared/forms/Question";
+
 const AskQuestion = () => {
-  return <div>Ask question</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
---
+++ components/shared/forms/Question.tsx
@@ -0,0 +1,182 @@
+/* eslint-disable prefer-promise-reject-errors */
+"use client";
+import { Editor } from "@tinymce/tinymce-react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { QuestionsSchema } from "@/lib/validation";
+
+const Question = () => {
+  // Define form handling directly within the component
+  const form = useForm<z.infer<typeof QuestionsSchema>>({
+    resolver: zodResolver(QuestionsSchema),
+    defaultValues: {
+      title: "",
+      explanation: "",
+      tags: [],
+    },
+  });
+
+  // Define the submit handler
+  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+    console.log(values); // Handle form submission
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5 ">
+                <Input
+                  placeholder="shadcn"
+                  {...field}
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Be Specific and imagine you&apos;re asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={() => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5 ">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  init={{
+                    plugins: [
+                      // Core editing features
+                      "anchor",
+                      "autolink",
+                      "charmap",
+                      "codesample",
+                      "emoticons",
+                      "image",
+                      "link",
+                      "lists",
+                      "media",
+                      "searchreplace",
+                      "table",
+                      "visualblocks",
+                      "wordcount",
+                      // Your account includes a free trial of TinyMCE premium features
+                      // Try the most popular premium features until Feb 4, 2025:
+                      "checklist",
+                      "mediaembed",
+                      "casechange",
+                      "export",
+                      "formatpainter",
+                      "pageembed",
+                      "a11ychecker",
+                      "tinymcespellchecker",
+                      "permanentpen",
+                      "powerpaste",
+                      "advtable",
+                      "advcode",
+                      "editimage",
+                      "advtemplate",
+                      "ai",
+                      "mentions",
+                      "tinycomments",
+                      "tableofcontents",
+                      "footnotes",
+                      "mergetags",
+                      "autocorrect",
+                      "typography",
+                      "inlinecss",
+                      "markdown",
+                      "importword",
+                      "exportword",
+                      "exportpdf",
+                    ],
+                    toolbar:
+                      "undo redo | codesample | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                    tinycomments_mode: "embedded",
+                    tinycomments_author: "Author name",
+                    mergetags_list: [
+                      { value: "First.Name", title: "First Name" },
+                      { value: "Email", title: "Email" },
+                    ],
+                    ai_request: (
+                      request: unknown,
+                      respondWith: {
+                        string: (arg0: () => Promise<never>) => unknown;
+                      }
+                    ) =>
+                      respondWith.string(() =>
+                        Promise.reject("See docs to implement AI Assistant")
+                      ),
+                  }}
+                  initialValue=""
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags<span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5 ">
+                <Input
+                  placeholder="add tags ..."
+                  {...field}
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add upto 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Question;
