f687e24cc5406ab9fca114b2c47ecaca7e312919
karan <karansawan5050@gmail.com>
2025-01-20 14:19:01 +0530

    add right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,8 @@
 // import Navbar from "@/components/shared/Navbar";
-import LeftSideBar from "@/components/shared/LeftSideBar";
+import LeftSideBar from "@/components/shared/Sidebars/LeftSideBar";
 import Navbar from "@/components/shared/Navbar/Navbar.tsx";
 import React from "react";
+import RightSideBar from "@/components/shared/Sidebars/RightSideBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,7 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        rightsidebar
+        <RightSideBar />
       </div>
       Toaster
     </main>
